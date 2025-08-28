4230268a017f6bf468b42190cc43252f53ba5d99
Waldey <waldeypereira@gmail.com>
2025-08-28 15:37:31 +0200

    🎉 Initial commit: setup project structure for cruddemo

---
+++ src/test/java/com/wfrocha/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wfrocha.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
